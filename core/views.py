9d0300c076338d578c68999bf090e756f0785113
wijihoon123 <wijihoon123@naver.com>
2022-04-23 12:18:15 +0900

    검색 GET 통신 추가

--- core/views.py
+++ core/views.py
@@ -5,10 +5,34 @@ from . import forms
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        return render(request, 'index.html')
+    if request.method == 'GET':
+        print(request)
+        return render(request, 'index.html')
 
 
 def sign_up(request):
+    form = forms.SignUpForm
+
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+        print(request)
+
+        if form.is_valid():
+            email = form.cleaned_data.get('email').lower()
+
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('/')
+
+    return render(request, 'sign_up.html', {'form': form})
+
+
+def recommendation(request):
     form = forms.SignUpForm
 
     if request.method == 'POST':
