75a5bac8064ac3291100b72736d7f0dcd19df5be
wijihoon123 <wijihoon123@naver.com>
2022-05-06 15:34:19 +0900

    검색 GET 통신 수정

--- core/views.py
+++ core/views.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 from itertools import permutations
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-# from tqdm.notebook import tqdm
 import urllib
 
 
@@ -72,16 +71,6 @@ def home(request):
 
                 # 코사인 유사도 확인
                 cosine_poi = get_cosine_top5(jeju_poi.copy(), name).reset_index(drop=True)
-                # row['thumbnail'],o row['kakao_grade'], row['kakao_review'], row['기타'], row['상호명'], row['input_sim']
-                key = zip(value for value in cosine_poi.to_dict('list').items())
-
-                print(key)
-                #for a, b, c, d, e, f, in key:
-                #    print(a)
-
-                #print(key = value for key, value in cosine_poi.to_dict('list').items())
-
-                #print(zip(value for key, value in cosine_poi.to_dict('list').items()))
                 json_records = cosine_poi.to_json(force_ascii=False, orient ='records')
                 data = json.loads(json_records)
 
